82ad9310eef138d96df172f3f7e5eef61d8d339e
FrKev1 <francisco.kevin37@gmail.com>
2020-09-30 15:32:48 +0200

    Ajout de lintéraction du menu

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("myBtn").onclick = function() {myFunction()};
 
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+const boutonMenu = document.querySelector(".bouton-menu");
+// We create a variable dropdownMenu set to the DOM element
+const menuDeroulant = document.querySelector(".menu-deroulant");
+
+// We listen for a click event
+boutonMenu.addEventListener("click", function() {
+  // On click we toggle the class visible on the dropdown
+  menuDeroulant.classList.toggle("visible");
+});
+
+

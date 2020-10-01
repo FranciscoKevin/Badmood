0452782279bac843e624a9abb4574e9a2e2bece7
Morgane Grénès <66020799+MorganeGrenes@users.noreply.github.com>
2020-10-01 15:47:47 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang ="fr">
+<head>
+	<meta charset="UTF-8">
+    <title>BadMood</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="footer.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Useful site to find informations if you need help about mental disorders.">
+</head>
+<body>
+    <div class='imageCard'>
+        <div class="textOnImage">
+            <p class="presentation">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nam laoreet felis vel eros convallis condimentum. Vestibulum nunc mauris, venenatis vel lobortis eget, scelerisque nec turpis. 
+                Vivamus pharetra vel diam placerat tempus.
+            </p>
+            <p class="presentation">
+                Nam at lectus eu nisl congue vestibulum ut eget turpis. 
+                Proin interdum neque erat, eget vestibulum libero tristique sed. Vivamus consequat rutrum nisl et aliquam. 
+                Morbi enim diam, malesuada nec consectetur eget, consectetur eget turpis.
+            </p>
+        </div>
+    </div>
+        <div class='parentCard'>
+            <div class='childCard'>
+                <div class='indexTitles'>
+                    <h2>METHODES</h2>
+                </div>
+                <article class="methodes">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nam laoreet felis vel eros convallis condimentum. Vestibulum nunc mauris, venenatis vel lobortis eget, scelerisque nec turpis. 
+                        Vivamus pharetra vel diam placerat tempus.
+                        Suspendisse potenti. Nam at lectus eu nisl congue vestibulum ut eget turpis. 
+                        Proin interdum neque erat, eget vestibulum libero tristique sed. Vivamus consequat rutrum nisl et aliquam. 
+                        Morbi enim diam, malesuada nec consectetur eget, consectetur eget turpis.
+                    </p>
+                </article>
+            </div>
+        </div>     
+        <div class='parentCard'>
+            <div class='childCard'>   
+                <iframe id="ytplayer" type="text/html" width="640" height="360" frameborder="0"
+                        src="https://www.youtube.com/embed/0Yr4hyFSJPk?autoplay=0&mute=1&controls=2&origin=http://example.com">
+                </iframe>
+            </div>
+        </div> 
+            <div class='parentCard'>
+                <div class='childCard'>
+                    <div class='indexTitles'>
+                        <h2>Besoin d'aide</h2>
+                    </div>
+                    <article class="needHelp">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Nam laoreet felis vel eros convallis condimentum. Vestibulum nunc mauris, venenatis vel lobortis eget, scelerisque nec turpis. 
+                            Vivamus pharetra vel diam placerat tempus.
+                            Suspendisse potenti. Nam at lectus eu nisl congue vestibulum ut eget turpis. 
+                            Proin interdum neque erat, eget vestibulum libero tristique sed. Vivamus consequat rutrum nisl et aliquam. 
+                            Morbi enim diam, malesuada nec consectetur eget, consectetur eget turpis.
+                        </p>
+                    </article>
+                </div>
+            </div>
+               
+	</section>
+        <footer>
+            <div class="titleFooter">
+                <p>BadMood</p>
+            </div>
+            <div class='advertisement'>
+                <p>Le contenu de cette page est à titre informatif. BadMood.com n'offre pas de conseils médicaux, de diagnostic ou de traitement.</p>
+            </div>
+            <hr class="principal">
+            <div class='informationsFooter'>
+                <p class="footer">N° vert : 064649484161 </p>
+                <p class="footer">A propos</p>
+                <p class="footer">Mentions légales</p>
+                <p class="footer">Contact</p>
+            </div>
+            <div class="wcsCopyright">
+                <p class="wildCodeSchool">2020 - Développé par la Wild Code School. Tous droits réservés.</p>
+            </div>          
+        </footer>
+</body>
+</html>

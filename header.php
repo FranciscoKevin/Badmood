f90238977296124e1aa822526c74d4f003558125
KevinRoig <70197762+KevinRoig@users.noreply.github.com>
2020-10-01 16:32:18 +0200

    Header hors index, à "include" dans chaque page

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+		<!---- MENU NAVIGATION ----->
+			<a href="index.html"><h1>BadMood</h1></a>
+			<nav>
+				<div class="bouton-menu">
+					<div class="barre bar1"></div>
+					<div class="barre bar2"></div>
+					<div class="barre bar3"></div>
+				</div>
+		      	<div class="menu-deroulant">
+			        <a href="#home">Home</a>
+			    	<a href="#about">About</a>
+			    	<a href="#contact">Contact</a>
+		      	</div>
+		     </nav>
+		     
+  </header>

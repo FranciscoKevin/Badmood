821dd84413ebba126fe7f5a69ba1a24d920a0bac
MorganeGrenes <morganegrenes@gmail.com>
2020-10-01 12:21:16 +0200

    html/css on index & footer added

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+<footer>
+    <div class="titleFooter">
+        <p>BadMood</p>
+        <p>Le contenu de cette page est à titre informatif. BadMood.com n'offre pas de conseils médicaux, de diagnostic ou de traitement.</p>
+    </div>
+    <div class='informationsFooter'>
+        <p>N° vert : 064649484161 </p>
+        <p>A propos</p>
+        <p>Mentions légales</p>
+        <p>Contact</p>
+        <p>2020 - Développé par la Wild Code School. Tous droits réservés.</p>
+    </div>
+</footer>

86b80af97fa6eb8b9baa73f40682e70abd71c404
FLorianBlm <71270969+FLorianBlm@users.noreply.github.com>
2020-10-02 15:00:37 +0200

    Update page_adulte.php
    remise du code Html

--- page_adulte.php
+++ page_adulte.php
@@ -1,38 +1,61 @@
 <!DOCTYPE html>
-<html lang ="fr">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <title>BadMood - Bienvenue</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-
-	<header>
-		<!---- MENU NAVIGATION ----->
-			<nav>
-				<div class="bouton-menu">
-					<div class="barre"></div>
-					<div class="barre"></div>
-					<div class="barre"></div>
-				</div>
-		      	<div class="menu-deroulant">
-			        <a href="#home">Home</a>
-			    	<a href="#about">About</a>
-			    	<a href="#contact">Contact</a>
-		      	</div>
-		     </nav>
-		     <h1>BadMood</h1>
-  </header>
-    
-	<section>
-		<article>
-			
-		</article>
-	</section>
-	<footer>
-		
-	</footer>
-	<script src="script.js"></script>
-</body>
+<html lang="fr">
+    <head>
+        <meta charset="urt-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="styles" href="enfant.css">
+        <title>BadMood</title>
+    </head>
+    <body>
+        <section class="imageAdulte"> <!-- photo début page -->
+            <img src="https://cdn.pixabay.com/photo/2014/07/11/15/09/unhappy-389944_960_720.jpg" alt="adulte, triste">
+            <p class="mentionPhotoAdulte">Image par PDPics de Pixabay</p>
+        </section>
+        <section class="mentalyCareChild"> <!-- repèrer "symptôme"-->
+            <h1>Les Troubles chez les adultes</h1>
+            <div class="signalAlarm"> 
+                <p>
+                    Des symptômes peuvent être révélateur d'un mal-être, d'une inhibition 
+                    des potentialités d'épanouissement ou d'un dysfonctionnement interpersonnel.
+                    <br><br>
+                    C'est pourquoi, il est utile de les entendre comme un signal d'alarme, comme les 
+                    manifestations d'un besoin de changement ou/et d'aide. Il est possible d'agir et d'y remédier.
+                </p>
+                <br>
+                <p> Le mal être peut s'exprimer de façon très différentes selon les personnes. Cependant voici les "grandes lignes" qui peuvent être un bonne indice :
+                    <ul>
+                        <li>mal être physique (fatigue, atonie ou maladies...)</li>
+                        <li>mal-être émotionnel (susceptibilité, manque d'estime de soi, anxiété, difficulté à gérer sa colère)</li>
+                        <li>mal-être social : travail, relations familiales, sociales... </li>
+                    </ul>
+                </p>
+                <br>
+                <p>
+                    Cependant il est important d'avoir en tête, que seule des professionnels de santé qualifiés 
+                    peuvent construire un portait mental de la personne suite à de nombreuses séances.
+                    <br>
+                    Votre mal être n'est pas une fatalité ! <br>
+                    Martellons-le, il est possible d'agir et d'y remédier. 
+                </p>
+            </div>
+            <div> <!-- Témoignage -->
+                <p>Vous pouvez retrouver des témoignages de personne souffrantes de troubles ou de leurs entourages <a href="https://fondationdouglas.qc.ca/temoignage/">ici</a>.</p>
+            </div>
+            <br>
+            <div> <!-- partie prise de rendez-vous-->
+                <p>
+                    Si vous souhaitez prendre rendez-vous avec des professionnels de santé pour commencer à aller mieux. Nous vous conseillons 
+                    d'en parler à votre médicin qui pourra vous orienter vers les professionnels les plus adaptés pour votre problème.
+                </p>
+                <br><br>
+                <p>vous pouvez aussi vous référer à notre page <a>professionnels de santé</a> pour trouver la personne la plus adapté à vos soucis</p>
+                <p>rendez-vous avec un psychologue près de chez vous : <a href="https://www.doctolib.fr/psychologue/antony-antony">cliquez ici</a></p>
+                <p>rendez-vous avec un psychiatre près de chez vous : <a href="https://www.doctolib.fr/psychiatre/antony-antony">cliquez ici</a></p>        
+            </div>
+        </section>
+        <section class="imageAdulte"> <!-- photo fin de page-->
+            <img src="https://cdn.pixabay.com/photo/2017/10/10/16/33/shaman-2837843_960_720.jpg" alt="revivre">
+            <p class="mentionPhotoAdulte">Image par Алина Осипова de Pixabay</p>
+        </section>
+    </body>
 </html>

d255ca140f995239c6f4f95186609c2b6f2abae2
KevinRoig <70197762+KevinRoig@users.noreply.github.com>
2020-10-02 15:31:11 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const boutonMenu = document.querySelector(".bouton-menu");
-// We create a variable dropdownMenu set to the DOM element
-const menuDeroulant = document.querySelector(".menu-deroulant");
-
-// We listen for a click event
-boutonMenu.addEventListener("click", function() {
-  menuDeroulant.classList.toggle("visible");
-});

5c75aacdd452d9e4c5fcc0905fc6787c05f547a7
FLorianBlm <71270969+FLorianBlm@users.noreply.github.com>
2020-10-02 15:09:52 +0200

    Update page_enfant.php
    Remise du code html

--- page_enfant.php
+++ page_enfant.php
@@ -1,1 +1,52 @@
-
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="urt-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BadMood</title>
+    </head>
+    <body>
+        <section class="imageSadChild_First">
+            <img src="https://cdn.pixabay.com/photo/2013/11/28/09/51/sad-219722_960_720.jpg" alt="enfant, triste">
+            <p class="mentionPhoto">Image par PublicDomainPictures de Pixabay </p>
+        </section>
+        <section class="mentalyCareChild">
+            <h1>Les Troubles chez les enfants</h1>
+            <div class="signalAlarm">
+                <p>
+                    Les symptômes suivants sont souvent révélateurs d'un mal-être, d'une inhibition 
+                    des potentialités d'épanouissement ou d'un dysfonctionnement interpersonnel.
+                    <br><br>
+                    C'est pourquoi, il est utile de les entendre comme un signal d'alarme, comme les 
+                    manifestations d'un besoin de changement ou/et d'aide. Il est possible d'agir et d'y remédier.
+                </p>
+                <br>
+                <p>
+                    Exemple de signaux peuvant être observer :
+                    <ul>
+                        <li>Troubles du sommeil ou/et appétit</li>
+                        <li>Perte de contrôle de sphincters</li>
+                        <li>troubles de l'appétit avec ou sans nausées ou/et vomissements</li>
+                        <li>Changements de l'humeur</li>
+                        <li>Repli sur soi</li>
+                        <li>manque d'attention, de concentration, rêverie, avec ou 
+                        sans chute des résultats scolaires, perte des acquis antérieurs</li>
+                        <li>absence de confiance en soi</li>
+                        <li>manifestations psycho-somatiques</li>
+                    </ul>
+                </p>
+                <br>
+                <p>
+                    Cependant il est important d'avoir en tête, que seule des professionnels de santé qualifiés 
+                    peuvent construire un portait mental de la personne suite à de nombreuses séances.
+                    <br>
+                    Martellons-le, il est possible d'agir et d'y remédier. 
+                </p>
+            </div>
+        </section>
+        <section>
+            <img src="https://cdn.pixabay.com/photo/2017/07/07/04/09/girl-2480361_960_720.jpg" alt="mère, enfant, ensemble">
+            <p class="mentionPhoto">Image par 5540867 de Pixabay</p>
+        </section>
+    </body>
+</html>

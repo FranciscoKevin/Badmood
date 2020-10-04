b3ec9c94880a8d3ce85e8692cd3e87eff09ec341
Kevin Roig <kevinroig37@gmail.com>
2020-10-04 12:09:39 +0200

    création de titres du header vers les pages; création des liens vers les pages. Fonctionnel.

--- header.php
+++ header.php
@@ -7,11 +7,11 @@
     <label for="check" class="checkbtn">
       <i class="fas fa-bars"></i>  		<!-- icone menu dans un bouton checkbox-->
     </label>
-    <label class="logo">BadMood</label>
+    <a href="index.php"><label class="logo">BadMood</label></a>
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Home</a></li>
+      <li><a href="page_adulte.php">Renseignements pour l'adulte</a></li>
+      <li><a href="page_enfant.php">Renseignements pour l'enfant</a></li>
+      <li><a href="page_infoSysteme.php">Le système de santé en France</a></li>
+      <li><a href="page_contact.php">Nous contacter</a></li>
     </ul>
   </nav>

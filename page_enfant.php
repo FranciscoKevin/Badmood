40e0bc85e330082f5a6bbb38940693c8d3199507
BoulMaker <florian.boulme1@gmail.com>
2020-10-07 12:24:04 +0200

    continue css et ajout de photo

--- page_enfant.php
+++ page_enfant.php
@@ -19,7 +19,7 @@
         <main class="mainBox">
             <section class="imageChild">
                 <div class="blockPhoto">
-                    <img src="https://cdn.pixabay.com/photo/2013/11/28/09/51/sad-219722_960_720.jpg" alt="enfant, triste">
+                    <img src="./img/enfantTriste.jpg" alt="enfant, triste">
                     <div class="mentionPhoto">
                         <p class="mentionMessage">Image par PublicDomainPictures de Pixabay </p>
                     </div>
@@ -28,35 +28,39 @@
             <section class="mentalyCareChild">
                 <h1>Les Troubles chez les enfants</h1>
                 <div class="signalAlarm">
-                    <p>
-                        Les symptômes suivants sont souvent révélateurs d'un mal-être, d'une inhibition 
-                        des potentialités d'épanouissement ou d'un dysfonctionnement interpersonnel.
-                        <br><br>
-                        C'est pourquoi, il est utile de les entendre comme un signal d'alarme, comme les 
-                        manifestations d'un besoin de changement ou/et d'aide. Il est possible d'agir et d'y remédier.
-                    </p>
-                    <br>
-                    <p>
-                        Exemple de signaux peuvant être observer :
-                        <ul>
-                            <li>Troubles du sommeil ou/et appétit</li>
-                            <li>Perte de contrôle de sphincters</li>
-                            <li>troubles de l'appétit avec ou sans nausées ou/et vomissements</li>
-                            <li>Changements de l'humeur</li>
-                            <li>Repli sur soi</li>
-                            <li>manque d'attention, de concentration, rêverie, avec ou 
-                            sans chute des résultats scolaires, perte des acquis antérieurs</li>
-                            <li>absence de confiance en soi</li>
-                            <li>manifestations psycho-somatiques</li>
-                        </ul>
-                    </p>
-                    <br>
-                    <p>
-                        Cependant il est important d'avoir en tête, que seule des professionnels de santé qualifiés 
-                        peuvent construire un portait mental de la personne suite à de nombreuses séances.
-                        <br>
-                        Martellons-le, il est possible d'agir et d'y remédier. 
-                    </p>
+                    <div>
+                        <p>
+                            Les symptômes suivants sont souvent révélateurs d'un mal-être, d'une inhibition 
+                            des potentialités d'épanouissement ou d'un dysfonctionnement interpersonnel.
+                            <br><br>
+                            C'est pourquoi, il est utile de les entendre comme un signal d'alarme, comme les 
+                            manifestations d'un besoin de changement ou/et d'aide. Il est possible d'agir et d'y remédier.
+                        </p>
+                    </div><br>
+                    <div class="listeSignaux">
+                        <p>
+                            Exemple de signaux peuvant être observer :<br><br>
+                            <ul>
+                                <li>Troubles du sommeil ou/et appétit</li>
+                                <li>Perte de contrôle de sphincters</li>
+                                <li>troubles de l'appétit avec ou sans nausées ou/et vomissements</li>
+                                <li>Changements de l'humeur</li>
+                                <li>Repli sur soi</li>
+                                <li>manque d'attention, de concentration, rêverie, avec ou 
+                                sans chute des résultats scolaires, perte des acquis antérieurs</li>
+                                <li>absence de confiance en soi</li>
+                                <li>manifestations psycho-somatiques</li>
+                            </ul>
+                        </p>
+                    </div><br>
+                    <div>
+                        <p>
+                            Cependant il est important d'avoir en tête, que seule des professionnels de santé qualifiés 
+                            peuvent construire un portait mental de la personne suite à de nombreuses séances.
+                            <br>
+                            Martellons-le, il est possible d'agir et d'y remédier. 
+                        </p>
+                    </div>
                 </div>
                 <div class="temoignageEnf"> <!-- Témoignage -->
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/_Kz7BD98dEo" 
@@ -79,7 +83,7 @@
             </section>
             <section class="imageChild"> <!-- photo fin de page-->
                 <div class="blockPhoto">
-                    <img src="https://cdn.pixabay.com/photo/2017/07/07/04/09/girl-2480361_960_720.jpg" alt="mère, enfant, ensemble">
+                    <img src="./img/parentEnfantSoutien.jpg" alt="mère, enfant, ensemble">
                     <div class="mentionPhoto">
                         <p class ="mentionMessage">Image par 5540867 de Pixabay</p>
                     </div>

f52b17e9b14977830652a28fdf1cec52351373c6
Kevin Roig <kevinroig37@gmail.com>
2020-10-07 17:05:10 +0200

    modifs

--- page_enfant.php
+++ page_enfant.php
@@ -16,75 +16,60 @@
 <body>
     <?php include("header.php")?>
         <main class="mainBox">
-            <section class="imageChild">
-                <div class="blockPhoto">
-                    <img src="https://cdn.pixabay.com/photo/2013/11/28/09/51/sad-219722_960_720.jpg" alt="enfant, triste">
-                    <div class="mentionPhoto">
-                        <p class="mentionMessage">Image par PublicDomainPictures de Pixabay </p>
-                    </div>
-                </div>
-            </section>
-            <section class="mentalyCareChild">
-                <h1>Les Troubles chez les enfants</h1>
-                <div class="signalAlarm">
-                    <p>
-                        Les symptômes suivants sont souvent révélateurs d'un mal-être, d'une inhibition 
-                        des potentialités d'épanouissement ou d'un dysfonctionnement interpersonnel.
-                        <br><br>
-                        C'est pourquoi, il est utile de les entendre comme un signal d'alarme, comme les 
-                        manifestations d'un besoin de changement ou/et d'aide. Il est possible d'agir et d'y remédier.
-                    </p>
-                    <br>
-                    <p>
-                        Exemple de signaux peuvant être observer :
-                        <ul>
-                            <li>Troubles du sommeil ou/et appétit</li>
-                            <li>Perte de contrôle de sphincters</li>
-                            <li>troubles de l'appétit avec ou sans nausées ou/et vomissements</li>
-                            <li>Changements de l'humeur</li>
-                            <li>Repli sur soi</li>
-                            <li>manque d'attention, de concentration, rêverie, avec ou 
-                            sans chute des résultats scolaires, perte des acquis antérieurs</li>
-                            <li>absence de confiance en soi</li>
-                            <li>manifestations psycho-somatiques</li>
-                        </ul>
-                    </p>
-                    <br>
-                    <p>
-                        Cependant il est important d'avoir en tête, que seule des professionnels de santé qualifiés 
-                        peuvent construire un portait mental de la personne suite à de nombreuses séances.
-                        <br>
-                        Martellons-le, il est possible d'agir et d'y remédier. 
-                    </p>
-                </div>
-                <div class="temoignageEnf"> <!-- Témoignage -->
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/_Kz7BD98dEo" 
-                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-                    gyroscope; picture-in-picture" allowfullscreen>
-                    </iframe>
-                    <p>Vous pouvez retrouver des témoignages de personne souffrantes de troubles ou de leurs entourages <a href="https://fondationdouglas.qc.ca/temoignage/">ici</a>.</p>
-                </div>
-                <br>
-                <div class="rdvEnf"> <!-- partie prise de rendez-vous-->
-                    <p>
-                        Si vous souhaitez prendre rendez-vous avec des professionnels de santé pour commencer à aller mieux. Nous vous conseillons 
-                        d'en parler à votre médicin qui pourra vous orienter vers les professionnels les plus adaptés pour votre problème.
-                    </p>
-                    <br><br>
-                    <p>vous pouvez aussi vous référer à notre page <a>professionnels de santé</a> pour trouver la personne la plus adapté à vos soucis</p>
-                    <p>rendez-vous avec un psychologue près de chez vous : <a href="https://www.doctolib.fr/psychologue/antony-antony">cliquez ici</a></p>
-                    <p>rendez-vous avec un psychiatre près de chez vous : <a href="https://www.doctolib.fr/psychiatre/antony-antony">cliquez ici</a></p>        
-                </div>
-            </section>
-            <section class="imageChild"> <!-- photo fin de page-->
-                <div class="blockPhoto">
-                    <img src="https://cdn.pixabay.com/photo/2017/07/07/04/09/girl-2480361_960_720.jpg" alt="mère, enfant, ensemble">
-                    <div class="mentionPhoto">
+            <div class="imageChild">
+                <p class="mentionMessage">Image par PublicDomainPictures de Pixabay </p>
+            </div>
+                <section class="blocContainerMentalyCareChild">
+                    <div class="bloc element-0"><h1>Les Troubles chez les enfants</h1></div>
+                        <div class="bloc element-1">
+                            <p>
+                                Les symptômes suivants sont souvent révélateurs d'un mal-être, d'une inhibition 
+                                des potentialités d'épanouissement ou d'un dysfonctionnement interpersonnel.
+                                <br><br>
+                                C'est pourquoi, il est utile de les entendre comme un signal d'alarme, comme les 
+                                manifestations d'un besoin de changement ou/et d'aide. Il est possible d'agir et d'y remédier.<br><br>
+                            </p>
+                                Exemple de signaux peuvant être observés :
+                                    <li>Troubles du sommeil ou/et appétit</li>
+                                    <li>Perte de contrôle de sphincters</li>
+                                    <li>Troubles de l'appétit avec ou sans nausées ou/et vomissements</li>
+                                    <li>Changements de l'humeur</li>
+                                    <li>Repli sur soi</li>
+                                    <li>Manque d'attention, de concentration, rêverie, avec ou 
+                                    sans chute des résultats scolaires, perte des acquis antérieurs</li>
+                                    <li>Absence de confiance en soi</li>
+                                    <li>Manifestations psycho-somatiques</li>
+                            <p><br>
+                                Cependant il est important d'avoir en tête, que seule des professionnels de santé qualifiés 
+                                peuvent construire un portait mental de la personne suite à de nombreuses séances.
+                                <br><br>
+                                <b>Martellons-le, il est possible d'agir et d'y remédier.</b> 
+                            </p>
+                        </div>
+                        <div class="bloc element-2"> <!-- Témoignage -->
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/_Kz7BD98dEo" 
+                            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+                            gyroscope; picture-in-picture" allowfullscreen>
+                            </iframe>
+                            <p>Vous pouvez retrouver des témoignages de personne souffrantes de troubles ou de leurs entourages <a href="https://fondationdouglas.qc.ca/temoignage/">ici</a>.</p>
+                        </div>
+                        <div class="bloc element-3"> <!-- partie prise de rendez-vous-->
+                            <p>
+                                Si vous souhaitez prendre rendez-vous avec des professionnels de santé pour commencer à aller mieux. Nous vous conseillons 
+                                d'en parler à votre médicin qui pourra vous orienter vers les professionnels les plus adaptés pour votre problème.
+                            </p>
+                            <br><br>
+                            <p>vous pouvez aussi vous référer aux pages suivantes pour trouver un professionnel de santé et trouver la personne la plus adapté à vos soucis:<br><br></p>
+                            <p class="rdv">rendez-vous avec <a target="blank" href="https://www.doctolib.fr/psychologue/antony-antony">un psychologue près de chez vous.</a><br><br></p>
+                            <p class="rdv">rendez-vous avec <a target="blank" href="https://www.doctolib.fr/psychiatre/antony-antony">un psychiatre près de chez vous.</a></p>        
+                        </div>
+                        <div class="imageChild2 bloc element-4"> <!-- photo fin de page-->
+                            <div class="mentionMessage">
                         <p class ="mentionMessage">Image par 5540867 de Pixabay</p>
+                            </div>
                     </div>
-                </div>
-            </section>
-        <main>
+                </section>
+        </main>
         <?php include("footer.php")?>
     </body>
 </html> 

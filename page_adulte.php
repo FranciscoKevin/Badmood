23ab3864a6f52d80584a0001a479aeab8a8b46fa
Kevin Roig <kevinroig37@gmail.com>
2020-10-08 16:50:51 +0200

    modifs

--- page_adulte.php
+++ page_adulte.php
@@ -17,37 +17,38 @@
 <?php include("header.php")?>
     <main class="mainBox">
         <div class="imageAdult"> <!-- photo début page -->
+            <h1 class="titleTrouble">Les Troubles chez les adultes</h1>
             <p class="mentionMessage">Image par PDPics de Pixabay</p>
         </div>
             <section class="blocContainerMentalyCareAdult"> <!-- repèrer "symptôme"-->
-                <div class="bloc element-0"><h1>Les Troubles chez les adultes</h1></div>
-                    <div class="bloc element-1">
-                        <div class="liste"><p>
-                            Des symptômes peuvent être révélateur d'un mal-être, d'une inhibition 
-                            des potentialités d'épanouissement ou d'un dysfonctionnement interpersonnel.
-                            <br><br>
-                            C'est pourquoi, il est utile de les entendre comme un signal d'alarme, comme les 
-                            manifestations d'un besoin de changement ou/et d'aide. Il est possible d'agir et d'y remédier.<br><br>
-                        </p>
-                        <p>
-                            Le mal être peut s'exprimer de façon très différentes selon les personnes. Cependant voici les "grandes lignes" qui peuvent être un bonne indice :
-                                <br><br>
-                                <li>mal être physique (fatigue, atonie ou maladies...)</li>
-                                <li>mal-être émotionnel (susceptibilité, manque d'estime de soi, anxiété, difficulté à gérer sa colère)</li>
-                                <li>mal-être social : travail, relations familiales, sociales... </li>
-                        <p>
-                            Cependant il est important d'avoir en tête, que seule des professionnels de santé qualifiés 
-                            peuvent construire un portait mental de la personne suite à de nombreuses séances.
-                            <br>
-                            Votre mal être n'est pas une fatalité ! <br><br>
-                            <b>Martellons-le, il est possible d'agir et d'y remédier.</b> 
-                        </p>
-                        </div>
-                    </div>
+                <div class="bloc element-0"></div>
+                <div class="bloc element-1">
+                <div class="liste"><p>
+                    Des symptômes peuvent être révélateurs d'un mal-être, d'une inhibition 
+                    des potentialités d'épanouissement ou d'un dysfonctionnement interpersonnel.
+                    <br><br>
+                    C'est pourquoi, il est utile de les entendre comme un signal d'alarme, comme les 
+                    manifestations d'un besoin de changement ou/et d'aide. Il est possible d'agir et d'y remédier.<br><br>
+                </p>
+                <p>
+                    Le mal être peut s'exprimer de façon très différentes selon les personnes. Cependant voici les "grandes lignes" qui peuvent être un bonne indice :
+                        <br><br>
+                        <li>mal être physique (fatigue, atonie ou maladies...)</li>
+                        <li>mal-être émotionnel (susceptibilité, manque d'estime de soi, anxiété, difficulté à gérer sa colère)</li>
+                        <li>mal-être social : travail, relations familiales, sociales... </li>
+                <p><br><br>
+                    Cependant il est important d'avoir en tête, que seule des professionnels de santé qualifiés 
+                    peuvent construire un portait mental de la personne suite à de nombreuses séances.
+                    <br><br>
+                    Votre mal être n'est pas une fatalité ! <br><br>
+                    <b>Martellons-le, il est possible d'agir et d'y remédier.</b> 
+                </p>
+                </div>
+                </div>
                     <div class="bloc element-2"> <!-- Témoignage -->
                         <div class="video">
                         <iframe width="560" height="100%" src="https://www.youtube.com/embed/a6MQiB-ic3s" 
-                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
                         gyroscope; picture-in-picture" allowfullscreen>
                         </iframe>
                         </div>
@@ -58,13 +59,17 @@
                     </div>
                     <div class="bloc element-3"> <!-- partie prise de rendez-vous-->
                         <p>
-                            Si vous souhaitez prendre rendez-vous avec des professionnels de santé pour commencer à aller mieux. Nous vous conseillons 
-                            d'en parler à votre médicin qui pourra vous orienter vers les professionnels les plus adaptés pour votre problème.
+                            Vous souhaitez prendre rendez-vous avec des professionnels de santé pour commencer à aller mieux? 
                         </p>
-                        <br><br>
-                        <p>Vous pouvez aussi vous référer à notre page <a>professionnels de santé</a> pour trouver la personne la plus adaptée à vos soucis:<br><br></p>
-                        <p class="rdv">Rendez-vous avec un psychologue près de chez vous : <a href="https://www.doctolib.fr/psychologue/antony-antony">cliquez ici</a><br><br></p>
-                        <p class="rdv">Rendez-vous avec un psychiatre près de chez vous : <a href="https://www.doctolib.fr/psychiatre/antony-antony">cliquez ici</a></p>        
+                        <p>
+                            Nous vous conseillons d'en parler à votre médecin qui pourra vous orienter vers les professionnels les plus adaptés pour votre problème.
+                        </p>
+                    </div>    
+                    <div class="bloc element-5">
+                        <p class="rdv"><a target="_blank" href="https://www.doctolib.fr/psychologue/antony-antony">Rendez-vous avec un psychologue près de chez vous.</a></p>
+                    </div>
+                    <div class="bloc element-6">
+                        <p class="rdv"><a target="_blank" href="https://www.doctolib.fr/psychiatre/antony-antony">Rendez-vous avec un psychiatre près de chez vous.</a></p>        
                     </div>
                     <div class=" imageAdult2 bloc element-4"> <!-- photo fin de page-->
                         <p class="mentionPhotoAdulte">Image par Алина Осипова de Pixabay</p>

44ace560a9fc4b25e2778c7412e47a9479e67792
FrKev1 <francisco.kevin37@gmail.com>
2020-10-08 14:32:46 +0200

    Ajout script js animation scroll sur la Nav bar

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
     <!-- Bibilotheque de logo svg par exemple pour le bouton menu sandwich-->
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Useful site to find informations if you need help about mental disorders.">
 </head>
@@ -63,5 +64,6 @@
         </div>
     </main>
         <?php include('footer.php');?>
+        <script src="script.js"></script>
 </body>
 </html>
